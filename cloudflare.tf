2ac2a8b4a1218692223aa11159d565f8f4626b8b
DJ Sharma <daljit.sharma@ibm.com>
2021-04-25 11:26:56 -0400

    minor fixes

--- cloudflare.tf
+++ cloudflare.tf
@@ -8,7 +8,7 @@ data "cloudflare_zones" "dj_domain" {
 # A record for AWS server
 resource "cloudflare_record" "www" {
   zone_id = lookup(data.cloudflare_zones.dj_domain.zones[0], "id")
-  name    = "www"
+  name    = "djsharma.xyz"
   value   = aws_instance.web.public_ip
   type    = "A"
   ttl     = 1
